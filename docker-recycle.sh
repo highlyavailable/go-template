d51f034533a73773122c96a821bdb1763fc6ba8c
Peter Bryant <pwbryant1@gmail.com>
2024-07-27 10:16:57 -0500

    updated docker recycle

--- docker-recycle.sh
+++ docker-recycle.sh
@@ -1,12 +1,20 @@
+#!/bin/bash
+
+# Stop and remove all goapp containers
+echo "-> Stopping and removing all goapp containers"
 containers=$(docker ps -a --filter "name=goapp" -q)
 if [ -n "$containers" ]; then
   docker stop $containers
   docker rm $containers
 fi
 
+# Remove all goapp images
+echo "-> Removing all goapp images"
 images=$(docker images --filter "reference=goapp" -q)
 if [ -n "$images" ]; then
   docker rmi $images
 fi
 
+# Build and run the goapp container
+echo "-> Building and running the goapp container"
 docker-compose up --build -d
